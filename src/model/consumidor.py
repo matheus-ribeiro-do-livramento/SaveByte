1a2f01ef918267407d512de6f09e2526ab8d8b3d
Gahenrique64 <gabrielhenriquegonzaga645@gmail.com>
2026-09-15 23:28:24 -0300

    Create consumidor.py

---
+++ src/model/consumidor.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from usuario import Usuario
+
+class Consumidor(usuario):
+    def __init__(self, bloqueaAte: datetime, 
+                 preferenciasAlimentares: list, 
+                 penalidade: int):
+        super().__init__(dataCadastro, email, id, localizacao, nome, senha)
+        self._bloquadoAte: datetime
+        self.preferenciasAlimentares: list
+
+    @property
+    def bloqueadoAte(self):
+        return self._bloquadoAte
+
+    @bloqueadoAte.setter
+    def bloqueadoAte(self, bloqueadoAte: datetime):
+        self._bloquadoAte = datetime
