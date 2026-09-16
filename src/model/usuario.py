51c6281f83a85a4613cbfcc54db4069ea80a6bc0
matheus-ribeiro-do-livramento <matheusribeirodolivramento123@gmail.com>
2026-09-15 23:19:07 -0300

    Correção na implementação da composição

--- src/model/usuario.py
+++ src/model/usuario.py
@@ -1,14 +1,15 @@
 from abc import ABC
 from datetime import datetime
+from localizacao import Localizacao
 
 
 class Usuario(ABC):
-    def __init__(self, id: int, nome: str, email: str, senha_hash: str, localizacao: Localizacao,   data_cadastro: datetime = None):
+    def __init__(self, id: int, nome: str, email: str, senha_hash: str,  endereco: str, latitude: float, longitude: float,   data_cadastro: datetime = None):
         self._id = id
         self._nome = nome
         self._email = email
         self._senha_hash = senha_hash
-        self._localizacao = localizacao
+        self._localizacao = Localizacao(endereco, latitude, longitude)
         self._data_cadastro = data_cadastro or datetime.now()
 
 #Feito por Matheus
